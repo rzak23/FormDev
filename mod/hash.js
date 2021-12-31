2302aa44a3f436c61b898c57d08ecdd10e24bcca
Reza Akbar Hidayat <50098895+rzak23@users.noreply.github.com>
2021-12-31 23:34:11 +0800

    add modul hashing

---
+++ mod/hash.js
@@ -0,0 +1,18 @@
+var CryptoJS = require('crypto-js');
+
+function hashing(){
+    var plain = document.getElementById('text').value;
+
+    document.getElementById('md5').value = CryptoJS.MD5(plain);
+    document.getElementById('sha1').value = CryptoJS.SHA1(plain);
+    document.getElementById('sha2').value = CryptoJS.SHA256(plain);
+    document.getElementById('sha3').value = CryptoJS.SHA3(plain);
+}
+
+function reset(){
+    document.getElementById('text').value = "";
+    document.getElementById('md5').value = "";
+    document.getElementById('sha1').value = "";
+    document.getElementById('sha2').value = "";
+    document.getElementById('sha3').value = "";
+}
