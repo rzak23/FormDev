d873b15b3f2535e42abf35cd2d63abb7c0b8fb3a
Reza Akbar Hidayat <50098895+rzak23@users.noreply.github.com>
2022-01-03 23:19:07 +0800

    fixed width and heigth window

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     icon: __dirname+'/build/icon.png',
     webPreferences: {
       nodeIntegration: true,
