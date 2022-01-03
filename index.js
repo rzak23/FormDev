858c2c8623c51c3f66faf10419ffb8e337edce50
rzak23 <reza.akbar2396@gmail.com>
2022-01-03 15:11:03 +0800

    add icon

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname+'/build/icon.png',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
