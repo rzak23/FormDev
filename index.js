1126f1ca3da059725a7ea7cfaf8313742d558aa6
Reza Akbar Hidayat <50098895+rzak23@users.noreply.github.com>
2022-01-04 19:34:32 +0800

    for release

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  mainWindow.removeMenu();
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
