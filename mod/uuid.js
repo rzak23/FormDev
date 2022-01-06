b54997b782121a5aa7ba6ee12c21e9229aabcf94
rzak23 <reza.akbar2396@gmail.com>
2022-01-06 15:58:46 +0800

    add version uuid

--- mod/uuid.js
+++ mod/uuid.js
@@ -1,9 +1,17 @@
 function generate(){
-    const {v4: uuidv4} = require('uuid');
+    const {v4: uuidv4, v1: uuidv1} = require('uuid');
 
-    document.getElementById('result').value = uuidv4();
+    var version = document.getElementById('version').value;
+
+    if(version == "1"){
+        var rand = uuidv1();
+    }else if(version == "4"){
+        var rand = uuidv4();;
+    }
+    document.getElementById('result').value = rand;
 }
 
 function reset(){
     document.getElementById('result').value = "";
+    document.getElementById('version').value = 1;
 }
