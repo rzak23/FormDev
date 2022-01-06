64d2709ca1bf29c528ac282a5e38197987a9dffd
Reza Akbar Hidayat <50098895+rzak23@users.noreply.github.com>
2022-01-06 22:49:40 +0800

    add option capitalize

--- mod/uuid.js
+++ mod/uuid.js
@@ -2,16 +2,31 @@ function generate(){
     const {v4: uuidv4, v1: uuidv1} = require('uuid');
 
     var version = document.getElementById('version').value;
+    var capt = document.getElementById('capital').checked;
 
-    if(version == "1"){
-        var rand = uuidv1();
-    }else if(version == "4"){
-        var rand = uuidv4();;
+    if(capt){
+        if(version == "1"){
+            var rand = uuidv1();
+            var result = rand.toUpperCase();
+        }else if(version == "4"){
+            var rand = uuidv4();
+            var result = rand.toUpperCase();
+        }
+    }else{
+        if(version == "1"){
+            var rand = uuidv1();
+            var result = rand
+        }else if(version == "4"){
+            var rand = uuidv4();
+            var result = rand
+        }
     }
-    document.getElementById('result').value = rand;
+
+    document.getElementById('result').value = result;
 }
 
 function reset(){
     document.getElementById('result').value = "";
     document.getElementById('version').value = 1;
+    document.getElementById('capital').checked = false;
 }
