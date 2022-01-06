b09dc7d5643b601c3dd5371a277177d5b9caee24
rzak23 <reza.akbar2396@gmail.com>
2022-01-06 15:39:59 +0800

    add option

--- mod/pasgen.js
+++ mod/pasgen.js
@@ -2,16 +2,29 @@ function myPass(){
     var alphas = "abcdefghijklmnopqrstu";
     var alphab = "ABCDEFGHIJKLMNOPQRSTU";
     var numeric = "1234567890";
-    var char = alphas+alphab+numeric;
+    var char = "!@#$%^&*()-_=+{}[];:,.<>/?`~";
+    
+    var option = document.getElementById('option').value;
+    var long = document.getElementById('panjang').value;
 
-    var rs = randomString(char);
+    if(option == "alpha"){
+        var mix =  alphas;
+    }else if(option == "AlPha"){
+        var mix = alphas+alphab;
+    }else if(option == "alphanumeric"){
+        var mix = alphas+alphab+numeric;
+    }else if(option == "alphanumchar"){
+        var mix = alphas+alphab+numeric+char;
+    }
+
+    var rs = randomString(mix,long);
     
     document.getElementById('pass').value = rs;
 }
 
-function randomString(char) {
-    var result           = '';
-    var length = 8;
+function randomString(char,long) {
+    var result = '';
+    var length = long;
     var clenght = char.length;
     for ( var i = 0; i < length; i++ ) {
         result += char.charAt(Math.floor(Math.random() * clenght));
@@ -21,4 +34,6 @@ function randomString(char) {
 
 function reset(){
     document.getElementById('pass').value = "";
+    document.getElementById('option').value = "alpha";
+    document.getElementById('panjang').value = 6;
 }
