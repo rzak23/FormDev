36e9e8a1e03b0de3b10192be4af628d61dd2eb00
rzak23 <reza.akbar2396@gmail.com>
2022-02-23 17:00:53 +0800

    tambah modul pembanding teks

---
+++ mod/banding.js
@@ -0,0 +1,29 @@
+function cekText(){
+    require('colors');
+    const Diff = require('diff');
+
+    const old = document.getElementById('lama').value;
+    const baru = document.getElementById('baru').value;
+    var hasil = document.getElementById('hasil');
+    const fragment = document.createDocumentFragment();
+
+    hasil.innerHTML = "";
+
+    const diff = Diff.diffChars(old,baru);
+    diff.forEach((part) => {
+        // green for additions, red for deletions
+        // grey for common parts
+        const color = part.added ? 'green' : part.removed ? 'red' : 'white';
+        span = document.createElement('span');
+        span.style.color = color;
+        span.appendChild(document.createTextNode(part.value));
+        fragment.appendChild(span);
+    });
+    hasil.appendChild(fragment);
+}
+
+function reset(){
+    document.getElementById('lama').value = "";
+    document.getElementById('baru').value = "";
+    document.getElementById('hasil').innerHTML = "";
+}
