0e20ea906c426af7e9658613eebf6d3e4d6b6448
Reza Akbar Hidayat <reza.akbar2396@gmail.com>
2022-03-23 00:30:29 +0800

    add new modul url-encoder-decoder

---
+++ mod/url-ende.js
@@ -0,0 +1,18 @@
+function encodeurl(){
+    var url = document.getElementById('url').value;
+    var enc = encodeURIComponent(url);
+
+    document.getElementById('output').value = enc;
+}
+
+function decodeurl(){
+    var url = document.getElementById('url').value;
+    var dcd = decodeURIComponent(url);
+
+    document.getElementById('output').value = dcd;
+}
+
+function reset(){
+    document.getElementById('url').value = "";
+    document.getElementById('output').value = "";
+}
