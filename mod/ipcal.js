c0f65b8be48418473644d0426ef21252481f9551
Reza Akbar Hidayat <reza.akbar2396@gmail.com>
2022-03-27 23:41:23 +0800

    tambah modul IP Calculator

---
+++ mod/ipcal.js
@@ -0,0 +1,33 @@
+function calIp(){
+    var NetMask = require('netmask').Netmask;
+    var ip = document.getElementById('ip').value;
+    var sub = document.getElementById('sub').value;
+
+    var comb = ip+sub;
+    var block = new NetMask(comb);
+
+    var base = block.base;
+    var netmask = block.mask;
+    var broadcast = block.broadcast;
+    var first = block.first;
+    var last = block.last;
+    var tblock = block.size;
+
+    document.getElementById('base').innerHTML = base;
+    document.getElementById('subnet').innerHTML = netmask;
+    document.getElementById('broadcast').innerHTML = broadcast;
+    document.getElementById('first').innerHTML = first;
+    document.getElementById('last').innerHTML = last;
+    document.getElementById('ipall').innerHTML = tblock-2;
+}
+
+function reset(){
+    document.getElementById('ip').value = "";
+    document.getElementById('sub').value = "";
+    document.getElementById('base').innerHTML = "";
+    document.getElementById('subnet').innerHTML = "";
+    document.getElementById('broadcast').innerHTML = "";
+    document.getElementById('first').innerHTML = "";
+    document.getElementById('last').innerHTML = "";
+    document.getElementById('ipall').innerHTML = "";
+}
