c3678ce201ad3ff902c5ac99ba809a0296520aac
Reza Akbar Hidayat <reza.akbar2396@gmail.com>
2022-06-01 23:16:16 +0800

    tambah fungsi distribusi per karakter

--- mod/checker.js
+++ mod/checker.js
@@ -8,10 +8,26 @@ function calculated(){
 	document.getElementById("karakter").innerHTML = h;
     document.getElementById("kata").innerHTML = s.split(' ').length;
     document.getElementById("kalimat").innerHTML = s.split('.').length;
+
+    // per char
+    var obj = {};
+    var pc = "";
+    
+    for(var i=0; i < text.length; i++){
+        var l = text.charAt(i);
+        obj[l] = (isNaN(obj[l]) ? 1 : obj[l] + 1);
+    }
+
+    for(var key in obj){
+        pc += key+" : "+obj[key]+"<br>";
+    }
+    document.getElementById('perchar').innerHTML = pc;
 }
 
 function reset(){
     document.getElementById('teks').value = "";
     document.getElementById('karakter').innerHTML = "";
     document.getElementById('kata').innerHTML = "";
+    document.getElementById('kalimat').innerHTML = "";
+    document.getElementById('perchar').innerHTML = "";
 }
