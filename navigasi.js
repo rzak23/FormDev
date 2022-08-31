92d6ec6205ad7528598833b67055587a5398f290
rzak23 <reza.akbar2396@gmail.com>
2022-08-31 12:06:53 +0800

    move information function

--- navigasi.js
+++ navigasi.js
@@ -279,3 +279,14 @@ function text_check_page(){
 	// Append to the `head` element
 	document.head.appendChild(script);
 }
+
+const {shell} = require('electron');
+function openDev(){
+    var url = "https://github.com/veler/DevToys";
+    shell.openExternal(url)
+}
+
+function openRepo(){
+    var url = "https://github.com/rzak23/FormDev";
+    shell.openExternal(url)
+}
