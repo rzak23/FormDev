141bcf33179312c249cdc89c6615f6cad74bc92f
rzak23 <50098895+rzak23@users.noreply.github.com>
2024-10-19 10:25:31 +0800

    rapikan struktur file developement

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const { src, dest, watch } = require('gulp');
 const fileInclude = require('gulp-file-include');
+const flatten = require('gulp-flatten');
 
 // Task untuk memproses file HTML
 function html() {
-  return src(['src/*.html']) // Source file HTML
+  return src(['src/**/*.html', '!src/partials/**']) // Source file HTML
     .pipe(fileInclude({
       prefix: '@@', // Default prefix untuk include
       basepath: '@file' // Relative path dari file
     }))
+    .pipe(flatten())
     .pipe(dest('pages')); // Output file HTML yang telah di-include
 }
 
