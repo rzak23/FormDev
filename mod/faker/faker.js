c09d36fdb087c2e757b148bb423aaa86b932005c
rzak23 <50098895+rzak23@users.noreply.github.com>
2025-02-20 17:02:35 +0800

    tambah faker email

--- mod/faker/faker.js
+++ mod/faker/faker.js
@@ -5,6 +5,13 @@ function faker_name(){
     document.getElementById('result').value = fullname;
 }
 
+function faker_email(){
+    const {faker} = require('@faker-js/faker');
+
+    let email = faker.internet.email();
+    document.getElementById('result').value = email;
+}
+
 function reset_form(){
     document.getElementById('form-faker').reset();
 }
