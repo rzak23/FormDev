f2d82adc1a0a9bba2e705341d204ea4478dd4500
rzak23 <reza.akbar2396@gmail.com>
2026-05-28 11:19:40 +0800

    update: initialize struktur code

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(title: "FormDev", minimumSize: Size(800, 600), center: true);
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold(body: Center(child: Text('Hello World!'))));
+  }
+}
